9bc2833430eb3d73848e36e138208b8d062551b4
Łukasz Smolaga <smolaga@gmail.com>
2015-11-14 22:14:07 +0100

    Update discovery service

--- DiscoveryService/src/main/java/org/semanticcloud/service/discovery/Application.java
+++ DiscoveryService/src/main/java/org/semanticcloud/service/discovery/Application.java
@@ -1,4 +1,4 @@
-package org.semanticcloud.discovery;
+package org.semanticcloud.service.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
