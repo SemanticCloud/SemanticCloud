e5ca09f97622f806585e2308981b7e7192f05d71
Łukasz Smolaga <smolaga@gmail.com>
2015-12-08 22:47:53 +0100

    Update style

--- Dashboard/src/main/webapp/scripts/app/account/login/login.js
+++ Dashboard/src/main/webapp/scripts/app/account/login/login.js
@@ -7,7 +7,7 @@ angular.module('dashboardApp')
                 parent: 'account',
                 url: '/login',
                 data: {
-                    authorities: [], 
+                    authorities: [],
                     pageTitle: 'login.title'
                 },
                 views: {
--- Dashboard/src/main/webapp/scripts/app/app.js
+++ Dashboard/src/main/webapp/scripts/app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('dashboardApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalprecht.translate', 
+angular.module('dashboardApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalprecht.translate',
                'ui.bootstrap', // for modal dialogs
     'ngResource', 'ui.router', 'ngCookies', 'ngAria', 'ngCacheBuster', 'ngFileUpload', 'infinite-scroll'])
 
@@ -14,12 +14,12 @@ angular.module('dashboardApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pasc
             if (Principal.isIdentityResolved()) {
                 Auth.authorize();
             }
-            
+
             // Update the language
             Language.getCurrent().then(function (language) {
                 $translate.use(language);
             });
-            
+
         });
 
         $rootScope.$on('$stateChangeSuccess',  function(event, toState, toParams, fromState, fromParams) {
@@ -32,12 +32,12 @@ angular.module('dashboardApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pasc
             if (toState.data.pageTitle) {
                 titleKey = toState.data.pageTitle;
             }
-            
+
             $translate(titleKey).then(function (title) {
                 // Change window title with translated one
                 $window.document.title = title;
             });
-            
+
         });
 
         $rootScope.back = function() {
@@ -79,7 +79,7 @@ angular.module('dashboardApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pasc
         $httpProvider.interceptors.push('authExpiredInterceptor');
         $httpProvider.interceptors.push('authInterceptor');
         $httpProvider.interceptors.push('notificationInterceptor');
-        
+
         // Initialize angular-translate
         $translateProvider.useLoader('$translatePartialLoader', {
             urlTemplate: 'i18n/{lang}/{part}.json'
@@ -93,5 +93,5 @@ angular.module('dashboardApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pasc
         tmhDynamicLocaleProvider.localeLocationPattern('bower_components/angular-i18n/angular-locale_{{locale}}.js');
         tmhDynamicLocaleProvider.useCookieStorage();
         tmhDynamicLocaleProvider.storageKey('NG_TRANSLATE_LANG_KEY');
-        
+
     });
